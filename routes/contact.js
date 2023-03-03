a40f10839453b3eaca74dc94d405b6bc4eecb5b2
andohfrancis9187gmail.com <andohfrancis9187@gmail.com>
2023-03-03 13:21:36 +0000

    email sending

--- routes/contact.js
+++ routes/contact.js
@@ -6,7 +6,7 @@ const sgMail = require('@sendgrid/mail');
 
 
 
-
+ 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -53,27 +53,56 @@ router.post('/', function(req, res){
             else{
              //   res.send("okay is working")
              res.render('contactsubmission', {email: email , phoneNumber: phoneNumber ,firstName:firstName , middleName:middleName ,lastName:lastName});
-             console.log(process.env.SENDGRID_API_KEY)
-             //after reander send email to client 
+            //  console.log(process.env.SENDGRID_API_KEY)
+            //  //after reander send email to client 
             
-               sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-                  const msg = {
-                  to: `${email}`,
-                  from: 'andohfrancis9187@gmail.com',
-                  templateId: 'd-9869dcbdb55a47d2acdaa13934d5f550',
-                  dynamicTemplateData: {
-                     subject: 'Testing Templates',
-                     name: `${firstName} ${ middleName} ${lastName}`,
-                     city: 'Ghana',
-                  },
-                  };
-                  sgMail.send(msg).then(()=>{
-                     console.log("email sent successfully")
-                  }).catch((err)=>{
-                        console.log(err)
-                  })
+            //    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            //       const msg = {
+            //       to: `${email}`,
+            //       from: 'andohfrancis9187@gmail.com',
+            //       templateId: 'd-9869dcbdb55a47d2acdaa13934d5f550',
+            //       dynamicTemplateData: {
+            //          subject: 'Testing Templates',
+            //          name: `${firstName} ${ middleName} ${lastName}`,
+            //          city: 'Ghana',
+            //       },
+            //       };
+            //       sgMail.send(msg).then(()=>{
+            //          console.log("email sent successfully")
+            //       }).catch((err)=>{
+            //             console.log(err)
+            //       })
+
+
+
+            //using nodemailer for sending messages
+            const nodemailer = require('nodemailer');
+
+               const transporter = nodemailer.createTransport({
+               service: 'gmail',
+               auth: {
+                  user: 'andohfrancis9187@gmail.com',
+                  pass: 'ipejtgjjarpugifu'
+                  // pass: '0558119187A' // naturally, replace both with your real credentials or an application-specific password
+               }
+               });
+
+               const mailOptions = {
+               from: 'andohfrancis9187@gmail.com',
+               to: 'andohfrancis1191@gmail.com',
+               subject: 'Invoices due',
+               text: 'Dudes, we really need your money.'
+               };
+
+               transporter.sendMail(mailOptions, function(error, info){
+               if (error) {
+                  console.log(error);
+               } else {
+                  console.log('Email sent: ' + info.response);
+               }
+               });
                   
-                     }                    }
+                     } }
                );
              }
 

b09da37931523c3d21efe193bbaeb01bd7dba427
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-03-17 00:26:00 +0000

    sendg

--- routes/contact.js
+++ routes/contact.js
@@ -59,81 +59,81 @@ router.post('/', function(req, res){
             //  console.log(process.env.SENDGRID_API_KEY)
             //  //after reander send email to client 
             
-            //    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-            //       const msg = {
-            //       to: `${email}`,
-            //       from: 'andohfrancis9187@gmail.com',
-            //       templateId: 'd-9869dcbdb55a47d2acdaa13934d5f550',
-            //       dynamicTemplateData: {
-            //          subject: 'Testing Templates',
-            //          name: `${firstName} ${ middleName} ${lastName}`,
-            //          city: 'Ghana',
-            //       },
-            //       };
-            //       sgMail.send(msg).then(()=>{
-            //          console.log("email sent successfully")
-            //       }).catch((err)=>{
-            //             console.log(err)
-            //       })
+               sgMail.setApi Key(process.env.SENDGRID_API_KEY);
+                  const msg = {
+                  to: `${email}`,
+                  from: 'andohfrancis9187@gmail.com',
+                  templateId: 'd-9869dcbdb55a47d2acdaa13934d5f550',
+                  dynamicTemplateData: {
+                     subject: 'Testing Templates',
+                     name: `${firstName} ${ middleName} ${lastName}`,
+                     city: 'Ghana',
+                  },
+                  };
+                  sgMail.send(msg).then(()=>{
+                     console.log("email sent successfully")
+                  }).catch((err)=>{
+                        console.log(err)
+                  })
 
 
 
             //using nodemailer for sending messages
 
-               const transporter = nodemailer.createTransport({
-               service: 'gmail',
-               auth: {
-                  user: 'andohfrancis9187@gmail.com',
-                  pass: 'ipejtgjjarpugifu'
-                  // pass: '0558119187A' // naturally, replace both with your real credentials or an application-specific password
-               }
-               });
+   //             const transporter = nodemailer.createTransport({
+   //             service: 'gmail',
+   //             auth: {
+   //                user: 'andohfrancis9187@gmail.com',
+   //                pass: 'ipejtgjjarpugifu'
+   //                // pass: '0558119187A' // naturally, replace both with your real credentials or an application-specific password
+   //             }
+   //             });
 
-            // Now we create an email template but first, we have to allow nodemail to compile the handlebars. 
-            //For this, we have a middleware transport.use()
-               const handlebarOptions = {
-                  viewEngine: {
-                     extName: '.hbs',
-                      partialsDir: path.resolve('./views/'),
-                      defaultLayout: false,
-                  },
-                  viewPath: path.resolve('./views/'),
-                  extName: '.hbs',
-              };
+   //          // Now we create an email template but first, we have to allow nodemail to compile the handlebars. 
+   //          //For this, we have a middleware transport.use()
+   //             const handlebarOptions = {
+   //                viewEngine: {
+   //                   extName: '.hbs',
+   //                    partialsDir: path.resolve('./views/'),
+   //                    defaultLayout: false,
+   //                },
+   //                viewPath: path.resolve('./views/'),
+   //                extName: '.hbs',
+   //            };
 
-              // use a template file with nodemailer
-               transporter.use('compile', hbs(handlebarOptions))
+   //            // use a template file with nodemailer
+   //             transporter.use('compile', hbs(handlebarOptions))
 
-               const mailOptions = {
-               from: 'andohfrancis9187@gmail.com',
-               to: `${email}`,
-               subject: 'Thank you for joining us',
-              template: 'email', // the name of the template file i.e email.handlebars
-               context:{
-                  name: `${firstName} ${middleName} ${lastName}`, // replace {{name}} with Adebola
+   //             const mailOptions = {
+   //             from: 'andohfrancis9187@gmail.com',
+   //             to: `${email}`,
+   //             subject: 'Thank you for joining us',
+   //            template: 'email', // the name of the template file i.e email.handlebars
+   //             context:{
+   //                name: `${firstName}  ${middleName} ${lastName}`, // replace {{name}} with Adebola
 
-               }
-               };
+   //             }
+   //             };
 
-               transporter.sendMail(mailOptions, function(error, info){
-               if (error) {
-                  console.log(error);
-               } else {
-                  console.log('Email sent: ' + info.response);
-               }
-               });
+   //             transporter.sendMail(mailOptions, function(error, info){
+   //             if (error) {
+   //                console.log(error);
+   //             } else {
+   //                console.log('Email sent: ' + info.response);
+   //             }
+   //             });
                   
-                     } }
-               );
+                   } }
+              );
              }
 
 
 
       }).catch(err => {
-         console.log(err)
+          console.log(err)
       })
 
-    }
+     }
    
      
 );
